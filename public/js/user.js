7b4175744327d0d26777216d519144a69afc4ab1
Meghanand Gejjela <97515984+Megh-Zyke@users.noreply.github.com>
2023-08-13 15:15:00 +0530

    Add files via upload

---
+++ public/js/user.js
@@ -0,0 +1,42 @@
+// const para = document.getElementsByClassName("post-info");
+// const Img = document.getElementsByClassName("img-Post");
+
+
+// Img[0].addEventListener("mouseover", (event) => {
+//   para[0].style.top = "-50px";
+//   console.log("i am here");
+// });
+
+
+// Img[0].addEventListener("mouseleave", (event) => {
+//     para[0].style.top = "0px";
+//     console.log("i am here");
+//   });
+
+//   para[0].addEventListener("mouseover", (event) => {
+//     para[0].style.top = "-50";
+//     console.log("i am here");
+//   });
+  
+  
+//   para[0].addEventListener("mouseleave", (event) => {
+//       para[0].style.top = "0";
+//       console.log("i am here");
+// });
+  
+
+
+// for(var i = 0 ; i<para.length ; i++){
+
+//     para[i].addEventListener("mouseover", (event) => {
+//         event.target.style.top = "-50px";
+//         console.log("i am here");
+//       });
+      
+      
+//       para[i].addEventListener("mouseleave", (event) => {
+//           event.target.style.top = "0px";
+//           console.log("i am here");
+//     });
+      
+// }
