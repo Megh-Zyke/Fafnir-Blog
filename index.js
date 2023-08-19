cfa21587d5817e2a78776786326a6047d8670ee8
Meghanand Gejjela <97515984+Megh-Zyke@users.noreply.github.com>
2023-08-19 23:02:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,9 +108,14 @@ app.get("/" , function(req, res){
 
 app.post("/posts" , function(req, res){
     var post = req.body.search;
+    
     var link = "/posts/" + post;
     
-        res.redirect(link);
+    if(link == "/posts/"){
+        res.redirect("/Main");
+    }
+    else{
+        res.redirect(link);}
 }); 
 
 
@@ -229,7 +234,7 @@ app.get("/user" , function(req, res){
 }
 });
 
-app.get("/users/:userId" , function(req, res){
+app.get("/user/:userId" , function(req, res){
     if(!req.isAuthenticated()){
         res.redirect("/login");
     }else{
